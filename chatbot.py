94662692fa626cbf9669d2a3d0de495c7d06bb4d
shifa786-cell <shifarizvi078@gmail.com>
2026-01-29 16:16:37 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,28 @@
+
+def chatbot_response(user_input):
+    user_input = user_input.lower().strip()
+
+    if user_input == "hello":
+        return "Hi!"
+    elif user_input == "how are you":
+        return "I'm fine, thanks!"
+    elif user_input == "bye":
+        return "Goodbye!"
+    else:
+        return "Sorry, I don't understand that."
+
+
+def run_chatbot():
+    print("Chatbot is running! Type 'bye' to exit.")
+
+    while True:
+        user_input = input("You: ")
+        response = chatbot_response(user_input)
+        print("Bot:", response)
+
+        if user_input.lower().strip() == "bye":
+            break
+
+
+
+run_chatbot()
